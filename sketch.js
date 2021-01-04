4e87ed97902bcfdc943263964b1ae2d940e4d499
SujinBb <36348220+SujinBb@users.noreply.github.com>
2021-01-05 01:05:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ var button;
 var _self;
 
 function setup() {
-  createCanvas(1280,800);
+  createCanvas(1350,700);
   background(255);
   button = createButton('GO')
   button.mousePressed(LinkClik);
-  button.position(580,420);
+  button.position(640,420);
   button.size(50,40);
  button.style("font-family", "Impact");
-  button.style("background-color","#fff");
+  button.style("background-color","#FFF");
  button.style("color","#000");
  button.style('font-size', '20px');
 }
@@ -24,17 +24,17 @@ function randomColor() {
 function draw() { 
   fill(255);
   textFont('Impact');
-  textSize(90);
+  textSize(130);
   textAlign(CENTER);
-  text('GENTLE MONSTER',620,380);
-  var d = random(0, 255);
+  text('PORTFOLIO',645,395);
+  var d = random(200, 255);
   var e = random(0, 255);
-  var f = random(0, 255);
+  var f = random(0, 225);
   fill(d,e,f);
   noStroke();
-  ellipse(mouseX, mouseY, 40, 40);
+  ellipse(mouseX, mouseY,200,200);
 }
 
 function LinkClik(){
- window.open("gentlemonster_portfolio.html", _self);
+ window.open("posterd.html", _self);
 }
